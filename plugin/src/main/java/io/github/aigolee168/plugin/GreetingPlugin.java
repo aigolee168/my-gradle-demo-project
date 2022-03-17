2829133860e756312ea89c6aa9f35b223c1f1e8e
limin <benxh_1989@163.com>
2022-03-17 14:45:45 +0800

    init commit

---
+++ plugin/src/main/java/io/github/aigolee168/plugin/GreetingPlugin.java
@@ -0,0 +1,13 @@
+package io.github.aigolee168.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class GreetingPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        System.out.println("message from greeting plugin");
+    }
+    
+}
